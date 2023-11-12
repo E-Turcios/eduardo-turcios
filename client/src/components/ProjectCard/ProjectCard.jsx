f912a8ae5a20cd95a7c781d75e98f0a6101fb7db
Eduardo Turcios <eddieturcios@outlook.com>
2023-11-12 12:24:29 -0500

    added basic project card components

---
+++ client/src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,10 @@
+export default function ProjectCard({
+  projectName,
+  technologies,
+  githubLink,
+  description,
+  status,
+  imgSrc,
+}) {
+  return <div data-testid="project-card"> card</div>;
+}
