14e1eb17c83308c4b3c20b2ef1268af932b3ef75
Eduardo Turcios <eddieturcios@outlook.com>
2023-11-12 14:44:38 -0500

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import ProjectCard from "./components/ProjectCard";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const props = {};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route
+        index
+        element={
+          <ProjectCard
+            projectName="some project"
+            technologies={["react"]}
+            githubLink="www.github.com"
+            description="just some project"
+            status={true}
+            imgSrc="lol.png"
+          />
+        }
+      />
+    </Route>
+  )
+);
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <ProjectCard />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
